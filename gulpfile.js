7dc5fed023aa585ebda653899550dd8d3787226b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-12-19 11:52:01 -0500

    ~ Gulp + Task 'watch': Observar permanentemente por cambios
    Aclaración:
    Observar permanentemente por cambios nos evita estar lanzando el
    comando gulp por una tarea específica. Haciendo que: observe por
    cambios, procese Sass a CSS continuamente.
    
    Para ejecutar esta tarea (en este caso particular) ejecutamos el
    siguiente comando en consola:
    
    $ gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 /* Invocamos/Importamos las dependencias que necesitamos */
 var gulp = require( 'gulp' ),
     sass = require( 'gulp-sass' ),
+    watch = require( 'gulp-watch' ),
     /* Configuración */ 
     path = {
         scss: './assets/scss',
@@ -13,5 +14,12 @@ gulp .task( 'style', ( done ) => {
         .pipe( sass() )                       /* Procesamos los archivos Sass a CSS */
             .on( 'error', sass .logError )    /* Captura los eventos (en este caso el evento 'error') */
         .pipe( gulp .dest( path .css ) );     /* Indicamos el destino de los archivos procesados */
+    done();
+});
+/* Task 'watch' */    
+gulp .task( 'watch', ( done ) => {
+    watch( path .scss + '/**/*.scss', () => {   /* Observa cambios en la ruta asignada (** Patrón goblin) */
+        gulp .start( 'style' );                 /* Inicia las tareas que se le indiquen (en este caso 'style') */
+    });       
     done();
 });
